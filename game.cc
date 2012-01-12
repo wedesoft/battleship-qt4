211253c848a703bf33114764fd515aefe791b5ab
Jan Wedekind <jan@wedesoft.de>
2012-01-12 21:33:03 +0000

    Ported random strategy to C++

--- game.cc
+++ game.cc
@@ -1,7 +1,13 @@
+#include <ctime>
+#include <cstdlib>
+#include <vector>
 #include "game.hh"
+
+using namespace std;
 
 Game::Game(void)
 {
+  srandom(time(NULL));
   m_human = new Player(this);
   m_computer = new Player(this);
   m_placing = true;
@@ -15,7 +21,15 @@ Game::~Game(void)
 
 void Game::computerMove(void)
 {
-
+  vector< pair< int, int> > unknown;
+  vector< pair< int, int> > prefer;
+  for (int y=0; y<N; y++)
+    for (int x=0; x<N; x++)
+      if (m_human->board(x, y) == UNKNOWN) {
+        unknown.push_back(pair< int, int >(x, y));
+      };
+  int idx = random() % unknown.size();
+  m_human->target(unknown[idx].first, unknown[idx].second);
 /*
   def computer_move
     unknown = []
