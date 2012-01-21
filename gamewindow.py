6268465cc6212cb360677c515f22826e38a44104
Jan Wedekind <jan@wedesoft.de>
2012-01-21 23:08:56 +0000

    Added a few slots

--- gamewindow.py
+++ gamewindow.py
@@ -6,4 +6,10 @@ class GameWindow(QMainWindow):
     self.ui = Ui_GameWindow()
     self.ui.setupUi(self)
     self.ui.actionQuit.triggered.connect(self.close)
+    self.ui.actionNewGame.triggered.connect(self.restart)
+    self.ui.startButton.clicked.connect(self.startGame)
+  def restart(self):
+    self.ui.startButton.setEnabled(True)
+  def startGame(self):
+    self.ui.startButton.setEnabled(False)
 
